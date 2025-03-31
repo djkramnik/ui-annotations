ae162f085699efcd24659fd6a269d5788b3a807a
David Gurr <davidk.gurr@gmail.com>
2025-03-31 19:38:52 -0400

    stubs

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,11 @@
 import dotenv = require('dotenv')
 import knex from 'knex'
 import path from 'path'
+import express from 'express'
+
 dotenv.config({ path: path.join(__dirname, '.env') })
 
 ;(async function main() {
-  console.log(process.env.DATABASE_PASSWORD)
   const db = knex({
     client: 'pg',
     connection: {
@@ -16,8 +17,24 @@ dotenv.config({ path: path.join(__dirname, '.env') })
   })
 
   try {
-    const resp = await db.raw('SELECT count(*) FROM annotations')
-    console.log('result:', resp?.rows?.[0])
+    await db.raw('SELECT count(*) FROM annotations')
+    // if we are here, we are connected to db
+    const app = express()
+    const port = process.env.PORT || 4000
+    if (!process.env.UI_DIRECTORY) {
+      throw Error('no UI_DIRECTORY environment variable')
+    }
+
+    app.use(express.static(process.env.UI_DIRECTORY))
+    app.use(express.json())
+
+    // TODO: define endpoint to fetch all annotations for a given url
+    // TODO: define endpoint to fetch all distinct urls for annotations
+
+    app.listen(port, () => {
+      console.log('serving listening on port', port)
+      console.log('serving static files from ', process.env.UI_DIRECTORY)
+    })
   } catch(err) {
     console.error(err)
   } finally {
