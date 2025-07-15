4db7aa45bf03206af4763fa88898277eb8b697c8
David Gurr <davidk.gurr@gmail.com>
2025-07-15 14:07:14 -0400

    I promise this works

--- packages/ui-labelling-extension/src/util.ts
+++ packages/ui-labelling-extension/src/util.ts
@@ -74,10 +74,96 @@ export function isInViewport({
   );
 }
 
+function getLegitimizedChildren(parent: HTMLElement): HTMLElement[] {
+  return Array.from(parent.children).filter(el => el instanceof HTMLElement)
+}
+
+// another version of this sits in contentScript but it has some non reusable shit in there
+// breadcrumbs saves the index of the element at each step so we may later
+// traverse down by the same path..
+function traverseUp({
+  origin,
+  distance,
+  breadcrumbs,
+}: {
+  origin: HTMLElement
+  distance: number
+  breadcrumbs?: number[]
+}): {
+  ancestor: HTMLElement | null
+  breadcrumbs?: number[]
+ } {
+  if (distance <= 0) {
+    return { ancestor: origin, breadcrumbs }
+  }
+  if (!origin.parentElement) {
+    return { ancestor: null, breadcrumbs }
+  }
+  return traverseUp({
+    origin: origin.parentElement,
+    distance: distance - 1,
+    breadcrumbs: breadcrumbs
+      ? breadcrumbs.concat([getLegitimizedChildren(origin.parentElement).indexOf(origin)])
+      : undefined
+  })
+}
+
+function traverseDown({
+  origin,
+  indexes,
+}: {
+  origin: HTMLElement,
+  indexes: number[]
+}): HTMLElement | null {
+  if (indexes.length < 1) {
+    return origin
+  }
+  const next = getLegitimizedChildren(origin)[indexes[0]]
+  if (!next) {
+    return null
+  }
+  return traverseDown({ origin: next, indexes: indexes.slice(1) })
+}
+
 export function getSibs(target: HTMLElement): HTMLElement[] {
   if (!target.parentElement) {
     return [ target ]
   }
-  return Array.from(target.parentElement.children)
-    .filter(el => el instanceof HTMLElement)
+  return getLegitimizedChildren(target.parentElement)
+}
+
+export function getCousins({
+  target,
+  distance,
+}: {
+  target: HTMLElement
+  distance: number
+}): HTMLElement[] {
+  const fallback = [ target ]
+  if (distance <= 0) {
+    console.log('what the hell')
+    return fallback
+  }
+  if (distance < 2) {
+    console.log('warning: you should have called getSibs instead')
+    return getSibs(target)
+  }
+  const { ancestor: grandMa, breadcrumbs } = traverseUp({
+    origin: target,
+    distance,
+    breadcrumbs: []
+  })
+
+  if (!grandMa) {
+    console.warn('missing grandparent for cousins')
+    return fallback
+  }
+
+  const aunts = getLegitimizedChildren(grandMa)
+
+  const reverseCrumbs = breadcrumbs!.slice(-1).reverse()
+
+  return aunts.map(auntie => {
+    return traverseDown({ origin: auntie, indexes: reverseCrumbs })
+  }).filter(couz => couz instanceof HTMLElement)
 }
